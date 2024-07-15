445d60c06f426ef446f5facaec4057d69a4c6257
VipinXD <vipinviswanathan2003@gmail.com>
2024-07-15 15:17:14 +0530

    new

---
+++ javascript/new.js
@@ -0,0 +1,11 @@
+var a=document.getElementById("para");
+document.write(a.textContent+"<br>");
+a.textContent="hello world";
+document.write(a.textContent+"<br>");
+a.style.background="red"
+var b=document.getElementsByClassName("main");
+document.write(b.textContent)
+x=10;
+y=20;
+c=x+y;
+document.write("<br>"+c)
